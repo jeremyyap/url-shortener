66fa570ca95ca48f10ba4fb3827d6394e0dd4731
Jeremy Yap <jeremy@estl.sg>
2020-05-05 22:31:00 +0800

    Add create endpoint

--- app/controllers/short_link_controller.rb
+++ app/controllers/short_link_controller.rb
@@ -1,10 +1,23 @@
 class ShortLinkController < ApplicationController
-  def create
+  skip_before_action :verify_authenticity_token
 
+  def create
+    short_link = ShortLink.new(new_link_params)
+    if short_link.save
+      render json: { slug: 'Testing' }
+    else
+      head :bad_request
+    end
   end
 
   def show
     short_link = ShortLink.find_by(slug: params[:slug])
     redirect_to short_link.original_url
+  end
+
+  private
+
+  def new_link_params
+    params.require(:link).permit(:original_url)
   end
 end
