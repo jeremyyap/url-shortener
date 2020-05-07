c579dceb232c5d43b7766bcc62d0dad28370407a
Jeremy Yap <jeremy@estl.sg>
2020-05-07 16:04:45 +0800

    Add protocol to url if missing to ensure absolute redirect

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,4 +1,5 @@
 class ShortLink < ApplicationRecord
+  before_validation :add_url_protocol
   after_initialize :set_defaults
 
   SLUG_LENGTH = 7
@@ -6,11 +7,11 @@ class ShortLink < ApplicationRecord
   # e.g. 0 vs O, 1 vs I
   SLUG_CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.chars.freeze
 
+  private
+
   def set_defaults
     self.slug ||= random_unique_slug
   end
-
-  private
 
   def random_unique_slug
     # TODO: Not guaranteed to be unique - race condition between here and saving
@@ -21,5 +22,11 @@ class ShortLink < ApplicationRecord
 
   def random_slug
     (0...SLUG_LENGTH).map { SLUG_CHARS.sample }.join
+  end
+
+  def add_url_protocol
+    uri = URI.parse(original_url)
+
+    self.original_url = 'https://' + original_url unless uri.scheme
   end
 end
